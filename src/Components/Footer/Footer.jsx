2e1063ebae59e7cf68ab0862f45d14b3a0a689e5
Vahagn <vahagngasparyan5@gmail.com>
2024-11-19 18:44:58 +0400

    changed a href to links in contact

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from './Footer.module.css'
 import logo_blue from '../Assets/prestige_okno_logo_blue.svg'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
 import { useNavigate } from "react-router-dom";
 
 
@@ -20,14 +20,14 @@ function Footer() {
                 
                 <img src={logo_blue} alt="Prestige Okno Logo"/>
                 <div className={classes.first_a_group}>
-                <a href="/">Главная</a>
-                <a href="/profiles">Профили</a>
-                <a href="/our_works">Наши работы</a>
+                <Link to="/">Главная</Link>
+                <Link to="/profiles">Профили</Link>
+                <Link to="/our_works">Наши работы</Link>
                 </div>
                 <div className={classes.second_a_group}>
-                <a>Новости и акции</a>
-                <a>Отзывы</a>
-                <a>Контакты</a>
+                <Link to="/discounts">Новости и акции</Link>
+                <Link to="reviews">Отзывы</Link>
+                <Link to="/contact">Контакты</Link>
                 </div>
                 <div className={classes.navigation_button}>
                 <button onClick={measurementPage}>Записаться на замер</button>
